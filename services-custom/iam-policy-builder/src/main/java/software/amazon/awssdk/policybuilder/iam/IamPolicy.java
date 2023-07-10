a1f0265dbd09a8afe8ce2c9c01a4fc9595b1691c
Matthew Miller <millem@amazon.com>
2023-07-10 14:06:16 -0700

    WIP

--- services-custom/iam-policy-builder/src/main/java/software/amazon/awssdk/policybuilder/iam/IamPolicy.java
+++ services-custom/iam-policy-builder/src/main/java/software/amazon/awssdk/policybuilder/iam/IamPolicy.java
@@ -21,32 +21,256 @@ import software.amazon.awssdk.policybuilder.iam.internal.DefaultIamPolicy;
 import software.amazon.awssdk.utils.builder.CopyableBuilder;
 import software.amazon.awssdk.utils.builder.ToCopyableBuilder;
 
+/**
+ * An AWS access control policy is a object that acts as a container for one or
+ * more statements, which specify fine grained rules for allowing or denying
+ * various types of actions from being performed on your AWS resources.
+ * <p>
+ * By default, all requests to use your resource coming from anyone but you are
+ * denied. Access control polices can override that by allowing different types
+ * of access to your resources, or by explicitly denying different types of
+ * access.
+ * <p>
+ * Each statement in an AWS access control policy takes the form:
+ * "A has permission to do B to C where D applies".
+ * <ul>
+ *   <li>A is the <b>principal</b> - the AWS account that is making a request to
+ *       access or modify one of your AWS resources.
+ *   <li>B is the <b>action</b> - the way in which your AWS resource is being accessed or modified, such
+ *       as sending a message to an Amazon SQS queue, or storing an object in an Amazon S3 bucket.
+ *   <li>C is the <b>resource</b> - your AWS entity that the principal wants to access, such
+ *       as an Amazon SQS queue, or an object stored in Amazon S3.
+ *   <li>D is the set of <b>conditions</b> - optional constraints that specify when to allow or deny
+ *       access for the principal to access your resource.  Many expressive conditions are available,
+ *       some specific to each service.  For example you can use date conditions to allow access to
+ *       your resources only after or before a specific time.
+ * </ul>
+ * <p>
+ * For more information, see <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/">The IAM User Guide</a>
+ *
+ * <h2>Usage Examples</h2>
+ * <b>Print an identity policy that allows a role to write items to an Amazon DynamoDB table.</b>
+ * {@snippet :
+ * IamPolicy policy =
+ *     IamPolicy.builder()
+ *              .addStatement(IamStatement.builder()
+ *                                        .effect(IamEffect.ALLOW)
+ *                                        .addAction("dynamodb:PutItem")
+ *                                        .addResource("arn:aws:dynamodb:us-east-2:123456789012:table/Books")
+ *                                        .build())
+ *              .build();
+ * System.out.println("Policy:\n" + policy.toJson());
+ * }
+ *
+ * <p>
+ * <b>Read an IAM policy and check to see if its actions are related to Amazon DynamoDB.</b>
+ * {@snippet :
+ * IamPolicy policy = IamPolicy.fromJson("""{
+ *   "Version": "2012-10-17",
+ *   "Statement": {
+ *     "Effect": "Allow",
+ *     "Action": "dynamodb:PutItem",
+ *     "Resource": "arn:aws:dynamodb:us-east-2:123456789012:table/Books"
+ *   }
+ * }""");
+ * policy.statements().forEach(statement -> {
+ *     Stream.concat(statement.actions().stream(),statement.notActions().stream()).forEach(action -> {
+ *         if (action.value().contains("dynamodb")) {
+ *             System.out.println("Action or NotAction involves DynamoDB: " + action.value());
+ *         }
+ *     });
+ * });
+ * }
+ *
+ * @see IamPolicyReader
+ * @see IamPolicyWriter
+ * @see IamStatement
+ */
 public interface IamPolicy extends ToCopyableBuilder<IamPolicy.Builder, IamPolicy> {
+    /**
+     * Create an {@code IamPolicy} from an IAM policy in JSON form.
+     * <p>
+     * This will raise an exception if the provided JSON is invalid or does not appear to represent a valid policy document.
+     * <p>
+     * This is equivalent to {@code IamPolicyReader.create().read(json)}.
+     */
     static IamPolicy fromJson(String json) {
         return IamPolicyReader.create().read(json);
     }
 
+    /**
+     * Create an {@code IamPolicy} containing the provided statements.
+     * <p>
+     * At least one statement is required.
+     * <p>
+     * This is equivalent to {@code IamPolicy.builder().statements(statements).build()}
+     */
     static IamPolicy create(Collection<IamStatement> statements) {
         return builder().statements(statements).build();
     }
 
+    /**
+     * Create a {@link Builder} for an {@code IamPolicy}.
+     */
     static Builder builder() {
         return new DefaultIamPolicy.Builder();
     }
 
+    /**
+     * Retrieve the value set by {@link Builder#id(String)}.
+     */
     String id();
+
+    /**
+     * Retrieve the value set by {@link Builder#version(String)}.
+     */
     String version();
+
+    /**
+     * Retrieve the value set by {@link Builder#statements(Collection)}.
+     */
     List<IamStatement> statements();
 
+    /**
+     * Convert this policy to the JSON format that is accepted by AWS services.
+     * <p>
+     * This is equivalent to {@code IamPolicyWriter.create().writeToString(policy)}
+     * <p>
+     * {@snippet :
+     * IamPolicy policy =
+     *     IamPolicy.builder()
+     *              .addStatement(IamStatement.builder()
+     *                                        .effect(IamEffect.ALLOW)
+     *                                        .addAction("dynamodb:PutItem")
+     *                                        .addResource("arn:aws:dynamodb:us-east-2:123456789012:table/Books")
+     *                                        .build())
+     *              .build();
+     * System.out.println("Policy:\n" + policy.toJson());
+     * }
+     */
     String toJson();
+
+    /**
+     * Convert this policy to the JSON format that is accepted by AWS services, using the provided writer.
+     * <p>
+     * This is equivalent to {@code writer.writeToString(policy)}
+     * <p>
+     * {@snippet :
+     * IamPolicyWriter prettyWriter =
+     *     IamPolicyWriter.builder()
+     *                    .prettyPrint(true)
+     *                    .build();
+     * IamPolicy policy =
+     *     IamPolicy.builder()
+     *              .addStatement(IamStatement.builder()
+     *                                        .effect(IamEffect.ALLOW)
+     *                                        .addAction("dynamodb:PutItem")
+     *                                        .addResource("arn:aws:dynamodb:us-east-2:123456789012:table/Books")
+     *                                        .build())
+     *              .build();
+     * System.out.println("Policy:\n" + policy.toJson(prettyWriter));
+     * }
+     */
     String toJson(IamPolicyWriter writer);
 
+    /**
+     * @see #builder()
+     */
     interface Builder extends CopyableBuilder<Builder, IamPolicy> {
+        /**
+         * Configure the {@code Id} element of the policy, specifying an optional identifier for the policy.
+         * <p>
+         * The ID is used differently in different services. ID is allowed in resource-based policies, but not in
+         * identity-based policies.
+         * <p>
+         * For services that let you set an ID element, we recommend you use a UUID (GUID) for the value, or incorporate a UUID
+         * as part of the ID to ensure uniqueness.
+         * <p>
+         * This value is optional.
+         * <p>
+         * {@snippet :
+         * IamPolicy policy =
+         *     IamPolicy.builder()
+         *              .id("cd3ad3d9-2776-4ef1-a904-4c229d1642ee")
+         *              // Additional fields
+         *              .build();
+         * }
+         *
+         * @see <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_id.html">Usage Guide</a>
+         */
         Builder id(String id);
+
+        /**
+         * Configure the {@code Version} element of the policy, specifying the language syntax rules that are to be used to
+         * process the policy.
+         * <p>
+         * By default, this value is {@code 2012-10-17}.
+         * <p>
+         * {@snippet :
+         * IamPolicy policy =
+         *     IamPolicy.builder()
+         *              .version("2012-10-17")
+         *              // Additional fields
+         *              .build();
+         * }
+         *
+         * @see
+         * <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_version.html">Usage Guide</a>
+         */
         Builder version(String version);
 
+        /**
+         * Configure the {@code Statement} element of the policy, specifying the access rules for this policy.
+         * <p>
+         * This will replace any other statements already added to the policy. At least one statement is required to
+         * create a policy.
+         * <p>
+         * {@snippet :
+         * IamPolicy policy =
+         *     IamPolicy.builder()
+         *              .statements(Arrays.asList(IamStatement.builder()
+         *                                                    .effect(IamEffect.DENY)
+         *                                                    .build()))
+         *              .build();
+         * }
+         * @see
+         * <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_statement.html">Usage Guide</a>
+         */
         Builder statements(Collection<IamStatement> statements);
+
+        /**
+         * Append a {@code Statement} element to this policy to specify additional access rules.
+         * <p>
+         * At least one statement is required to create a policy.
+         * <p>
+         * {@snippet :
+         * IamPolicy policy =
+         *     IamPolicy.builder()
+         *              .addStatement(IamStatement.builder()
+         *                                        .effect(IamEffect.DENY)
+         *                                        .build())
+         *              .build();
+         * }
+         * @see
+         * <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_statement.html">Usage Guide</a>
+         */
         Builder addStatement(IamStatement statement);
+
+        /**
+         * Append a {@code Statement} element to this policy to specify additional access rules.
+         * <p>
+         * This works the same as {@link #addStatement(IamStatement)}, except you do not need to specify {@code IamStatement
+         * .builder()} or {@code build()}. At least one statement is required to create a policy.
+         * <p>
+         * {@snippet :
+         * IamPolicy policy =
+         *     IamPolicy.builder()
+         *              .addStatement(s -> s.effect(IamEffect.DENY))
+         *              .build();
+         * }
+         * @see
+         * <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_statement.html">Usage Guide</a>
+         */
         Builder addStatement(Consumer<IamStatement.Builder> statement);
     }
 }
--- services-custom/iam-policy-builder/src/main/java/software/amazon/awssdk/policybuilder/iam/IamStatement.java
+++ services-custom/iam-policy-builder/src/main/java/software/amazon/awssdk/policybuilder/iam/IamStatement.java
@@ -22,19 +22,124 @@ import software.amazon.awssdk.policybuilder.iam.internal.DefaultIamStatement;
 import software.amazon.awssdk.utils.builder.CopyableBuilder;
 import software.amazon.awssdk.utils.builder.ToCopyableBuilder;
 
+/**
+ * A statement is the formal description of a single permission, and is always
+ * contained within a policy object.
+ * <p>
+ * A statement describes a rule for allowing or denying access to a specific AWS
+ * resource based on how the resource is being accessed, and who is attempting
+ * to access the resource. Statements can also optionally contain a list of
+ * conditions that specify when a statement is to be honored.
+ * <p>
+ * For example, consider a statement that:
+ * <ul>
+ * <li>allows access (the effect)
+ * <li>for a list of specific AWS account IDs (the principals)
+ * <li>when accessing an SQS queue (the resource)
+ * <li>using the SendMessage operation (the action)
+ * <li>and the request occurs before a specific date (a condition)
+ * </ul>
+ *
+ * <p>
+ * Statements takes the form:  "A has permission to do B to C where D applies".
+ * <ul>
+ *   <li>A is the <b>principal</b> - the AWS account that is making a request to
+ *       access or modify one of your AWS resources.
+ *   <li>B is the <b>action</b> - the way in which your AWS resource is being accessed or modified, such
+ *       as sending a message to an Amazon SQS queue, or storing an object in an Amazon S3 bucket.
+ *   <li>C is the <b>resource</b> - your AWS entity that the principal wants to access, such
+ *       as an Amazon SQS queue, or an object stored in Amazon S3.
+ *   <li>D is the set of <b>conditions</b> - optional constraints that specify when to allow or deny
+ *       access for the principal to access your resource.  Many expressive conditions are available,
+ *       some specific to each service.  For example you can use date conditions to allow access to
+ *       your resources only after or before a specific time.
+ * </ul>
+ *
+ * <p>
+ * There are many resources and conditions available for use in statements, and
+ * you can combine them to form fine grained custom access control polices.
+ *
+ * <p>
+ * Statements are typically attached to a {@link IamPolicy}.
+ *
+ * <p>
+ * For more information, see <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/">The IAM User Guide</a>
+ *
+ * <h2>Usage Examples</h2>
+ * <b>Create a statement that allows a role to write items to an Amazon DynamoDB table.</b>
+ * {@snippet :
+ * IamStatement statement =
+ *     IamStatement.builder()
+ *                 .effect(IamEffect.ALLOW)
+ *                 .addAction("dynamodb:PutItem")
+ *                 .addResource("arn:aws:dynamodb:us-east-2:123456789012:table/Books")
+ *                 .build();
+ * }
+ *
+ * <p>
+ * <b>Create a statement that denies access to all users.</b>
+ * {@snippet :
+ * IamStatement statement =
+ *     IamStatement.builder()
+ *                 .effect(IamEffect.DENY)
+ *                 .addPrincipal(IamPrincipal.ALL)
+ *                 .build();
+ * }
+ *
+ * @see IamPolicy
+ * <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_statement.html">Usage Guide</a>
+ */
 public interface IamStatement extends ToCopyableBuilder<IamStatement.Builder, IamStatement> {
+    /**
+     * Create a {@link Builder} for an {@code IamStatement}.
+     */
     static Builder builder() {
         return new DefaultIamStatement.Builder();
     }
 
+    /**
+     * Retrieve the value set by {@link Builder#sid(String)}.
+     */
     String sid();
+
+    /**
+     * Retrieve the value set by {@link Builder#effect(IamEffect)}.
+     */
     IamEffect effect();
+
+    /**
+     * Retrieve the value set by {@link Builder#principals(Collection)}.
+     */
     List<IamPrincipal> principals();
+
+    /**
+     * Retrieve the value set by {@link Builder#notPrincipals(Collection)}.
+     */
     List<IamPrincipal> notPrincipals();
+
+    /**
+     * Retrieve the value set by {@link Builder#actions(Collection)}.
+     */
     List<IamAction> actions();
+
+    /**
+     * Retrieve the value set by {@link Builder#notActions(Collection)}.
+     */
     List<IamAction> notActions();
+
+    /**
+     * Retrieve the value set by {@link Builder#resources(Collection)}.
+     */
     List<IamResource> resources();
+
+    /**
+     * Retrieve the value set by {@link Builder#notResources(Collection)}.
+     */
     List<IamResource> notResources();
+
+    /**
+     * Retrieve the value set by {@link Builder#conditions(Collection)}.
+     */
     List<IamCondition> conditions();
 
     interface Builder extends CopyableBuilder<Builder, IamStatement> {
